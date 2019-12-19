739a1bd4cf097d80f04d950134967b4defa9d30a
bdwkyy <bdwkyangyang@163.com>
2019-12-19 11:29:46 +0800

    Create fork.c

---
+++ fork.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main(){
+        pid_t pid;
+        if((pid = fork()) < 0){
+                perror("fork err");
+        } else if(pid == 0) {
+                if((pid = fork()) < 0){
+                        perror("fork err");
+                } else if(pid == 0){
+                        sleep(2);
+                        printf("second child,parent pid = %d\n",getppid());
+                        exit(0);
+                } else if(pid > 0){
+                        exit(0);
+                }
+        } else {
+                if(waitpid(pid,NULL,0) != pid)
+                        perror("waitpid");
+                exit(0);
+        }
+}
